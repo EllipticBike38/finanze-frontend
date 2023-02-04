10b68195519b107b1f0b48d01063fe7a276a4fdf
Andrea <andrea.mazzini00@gmail.com>
2023-02-04 14:52:24 +0100

    loading and scrollbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoadingService } from './loading.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-3';
+  loading:boolean=false
+  constructor(private loadingService: LoadingService) {
+    loadingService.currentLoadingBool.subscribe(bool => {this.loading=bool})
+    
+  }
 }
--- src/app/loading.service.ts
+++ src/app/loading.service.ts
@@ -7,12 +7,17 @@ import { BehaviorSubject } from 'rxjs';
 export class LoadingService {
 
   private LoadingBool = new BehaviorSubject(true);
+  private FirstLoad = new BehaviorSubject(true);
   currentLoadingBool = this.LoadingBool.asObservable();
+  currentFirstLoad  = this.LoadingBool.asObservable();
  
   constructor() {
  
   }
   updateLoading(message: boolean) {
+  this.LoadingBool.next(message)
+  }
+  updateFirstLoad(message: boolean) {
   this.LoadingBool.next(message)
   }
 }
